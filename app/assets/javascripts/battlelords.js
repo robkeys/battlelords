9daf6caabfd85367c5f9afadce6405f1969c729c
Rob Keys <robertckeys@gmail.com>
2017-05-28 12:40:47 -0800

    Refactor javascript for roll handling in new view
    Roll handling scripts were spread across a number of obtuse functions. Refactored into more logical functions and fixed a bug with selecting a roll in a selector that already had a value rather than clearing the value first. This cause the original value in the selector to be removed from the list until a value was cleared in any field. Also changed the rolls list to surround a selected value with the <del> tag rather than removing it from the list. This will allow more versatility when displaying or hiding selected rolls going forward.

--- app/assets/javascripts/battlelords.js
+++ app/assets/javascripts/battlelords.js
@@ -1,105 +1,68 @@
-// get & set cookies functions c/o: https://www.w3schools.com/js/js_cookies.asp
-function setCookie(cname, cvalue, exdays=2) {
-    var d = new Date();
-    d.setTime(d.getTime() + (exdays*24*60*60*1000));
-    var expires = "expires="+ d.toUTCString();
-    document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
-}
-
-function getCookie(cname) {
-    var name = cname + "=";
-    var decodedCookie = decodeURIComponent(document.cookie);
-    var ca = decodedCookie.split(';');
-    for(var i = 0; i <ca.length; i++) {
-        var c = ca[i];
-        while (c.charAt(0) == ' ') {
-            c = c.substring(1);
-        }
-        if (c.indexOf(name) == 0) {
-            return c.substring(name.length, c.length);
-        }
-    }
-    return "";
-}
-
-function setCookieVals() {
-    let vsRolls = $(".vs-rolls").data("vs-rolls");
-    setCookie("vsNameList", $(".vs-names").data("vs-names"));
-    setCookie("vsRolls", vsRolls);
-    setCookie("vsRollsLeft", vsRolls)
-}
 
 function displayRaceTable(raceID) {
-    let elements = document.querySelectorAll('.race-div')
+    let elements = document.querySelectorAll('.race-div');
     for (let i = 0; i < elements.length; i++) {
         elements[i].style.display = "none"
     }
     $('#race-' + raceID).css("display","block")
 }
 
-function getNames() {
-    let vsNames = getCookie("vsNameList");
-    return vsNames.split(',');
-}
-
-function getRollsLeft() {
-    let rolls = getCookie("vsRollsLeft");
-    return rolls.split(',').map(Number)
-}
-
-function getAllRolls() {
-    let rolls = getCookie("vsRolls");
-    return rolls.split(',').map(Number)
-}
-
-function remRoll(newVal) {
-    let vsRolls = getRollsLeft();
-    let i = vsRolls.indexOf(parseInt(newVal));
-    vsRolls.splice(i, 1);
-    return vsRolls
-}
-
-function addRoll(vsNames) {
+function getSelectedRolls(vsNames) {
+    console.log("*** RUNNING selectedRolls ***");
     let selRolls = [];
     let idPrefix = '#battlelord_base_vs_attributes_';
     while (vsNames.length) { selRolls.push($(idPrefix + vsNames.shift()).val())}
     selRolls = selRolls.map(Number).filter(function(e) {return e !== 0});
-    let vsRolls = getAllRolls();
-    while (selRolls.length) {
-        let i = vsRolls.indexOf(selRolls.pop());
-        vsRolls.splice(i, 1)
-    }
-    return vsRolls
+    return selRolls
 }
 
-function updateSelectors(vsNames, vsRolls) {
+function updateSelectors(vsNames, availRolls) {
+    console.log("*** RUNNING updateSelectors ***");
     while (vsNames.length) {
         let optionsHTML = "<option value=''></option>";
-        let currRolls = vsRolls.slice(0);
+        let currAvailRolls = availRolls.slice(0);
         let currID = '#battlelord_base_vs_attributes_' + vsNames.pop();
         let currVal = $(currID).val();
         if (currVal) {
             optionsHTML = optionsHTML + "<option value=" + currVal + " selected>" + currVal + "</option>\n"
         }
-        while (currRolls.length) {
-            currVal = currRolls.shift();
+        while (currAvailRolls.length) {
+            currVal = currAvailRolls.shift();
             optionsHTML = optionsHTML + "<option value=" + currVal + ">" + currVal + "</option>\n"
         }
         document.querySelector(currID).innerHTML = optionsHTML
     }
 }
 
-function updateRolls(newVal) {
-    let rollsList = '';
-    let vsNames = getNames();
-    let vsRolls = null;
-    if (newVal) {
-        vsRolls = remRoll(newVal)
-    } else {
-        vsRolls = addRoll(vsNames.slice(0))
+function updateScores(selRolls, allRolls) {
+    console.log("*** RUNNING updateScores ***");
+    let newHTML = "";
+    for (let n = 0; n < allRolls.length; n++ ) {
+        let newLine = "";
+        i = selRolls.indexOf(allRolls[n]);
+        if (i >= 0) {
+            newLine = "<del>" + allRolls[n] + "</del><br>";
+            selRolls.splice(i, 1);
+        } else {
+            newLine = allRolls[n] + "<br>"
+        }
+        newHTML = newHTML + newLine
     }
-    setCookie("vsRollsLeft", vsRolls);
-    updateSelectors(vsNames.slice(0), vsRolls.slice(0));
-    while (vsRolls.length) { rollsList = rollsList + vsRolls.shift() + "<br>"}
-    document.querySelector("#rolls-list-target").innerHTML = rollsList;
+    document.querySelector("#rolls-list-target").innerHTML = newHTML;
+}
+
+function updateRolls() {
+    console.log("*** RUNNING updateRolls ***");
+    let vsNames = $(".vs-names").data("vs-names").slice(0);
+    let selRolls = getSelectedRolls(vsNames.slice(0));
+    let allRolls = $(".vs-rolls").data("vs-rolls").slice(0);
+    let availRolls = allRolls.slice(0);
+    for (let n = 0; n < selRolls.length; n++) {
+        let i = availRolls.indexOf(selRolls[n]);
+        if (i >= 0) {
+            availRolls.splice(i, 1);
+        }
+    }
+    updateSelectors(vsNames.slice(0), availRolls.slice(0));
+    updateScores(selRolls.slice(0), allRolls.slice(0))
 }
